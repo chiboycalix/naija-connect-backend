4c7aa3c0262dd4fde2982c5f155415b42d64dcee
chiboycalix <igwechinonso77@gmail.com>
2023-05-24 16:45:02 +0100

    add login functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,7 +7,8 @@ export const UserRouter = express.Router();
 const userController = new UserController();
 
 UserRouter.post("/", authMiddleware, userController.createUser);
-UserRouter.get("/", authMiddleware, userController.getUsers);
+UserRouter.get("/", userController.getUsers);
+// UserRouter.get("/", authMiddleware, userController.getUsers);
 UserRouter.get("/:id", authMiddleware, userController.getUserById);
 UserRouter.put("/:id", authMiddleware, userController.updateUser);
 UserRouter.delete("/:id",authMiddleware, userController.deleteUser);
